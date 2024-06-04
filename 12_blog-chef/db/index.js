e8c8cfb3e312a1002d2253c69b7eb3bfb35ae69b
joechea <s.chea@aupp.edu.kh>
2024-06-04 17:31:38 +0700

    initial commit

---
+++ 12_blog-chef/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToDb = () =>
+  mongoose.connect(
+    `mongodb+srv://${process.env.atlasUser}:${process.env.atlasPassword}@cluster0.jdnga8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
+
+export default connectToDb;
